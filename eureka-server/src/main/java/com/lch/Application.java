64f14c9019573b21bd0d0d4ccadcdd72c9275ec2
lichanghong <lichanghong@xdf.cn>
2019-04-01 18:09:24 +0800

    第一次提交

---
+++ eureka-server/src/main/java/com/lch/Application.java
@@ -0,0 +1,18 @@
+package com.lch;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p>启动类</p>
+ *
+ * @author lichanghong  create by  2019/4/1 18:02
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
